321640fe8ccc6aaa55e53b4d3a888e9806b90db7
trainhui <850131804@qq.com>
2019-09-01 14:25:05 +0800

    trainhui2

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,7 @@ public class test {
     public static  void main(String []args){
         System.out.println("test01");
         System.out.println("test02");
+        System.out.println("test03----bygitdemo02");
     }
 
 }
